7be6eae109a7a8fca95df0f60e20017ad0582656
koromodako <koromodako@gmail.com>
2021-07-28 00:47:55 +0200

    Log to stderr

--- timeline_explorer/logging.py
+++ timeline_explorer/logging.py
@@ -1,12 +1,15 @@
 '''Logging module
 '''
+from rich.console import Console
 from rich.logging import RichHandler
 from logging import getLogger
 
 
 LOGGER = getLogger('timeline-explorer')
 LOGGER.setLevel('INFO')
-LOGGER.addHandler(RichHandler(level='DEBUG'))
+LOGGER.addHandler(
+    RichHandler(level='DEBUG', console=Console(stderr=True)
+)
 
 
 def log_enable_debug(enable=True):
